e96cedba73f36ddf6e8906991d753a927d074b77
HG <appwhy@qq.com>
2025-03-16 15:35:54 +0800

    使用公共库

--- tetris/v2.cpp
+++ tetris/v2.cpp
@@ -11,6 +11,7 @@
 #include <stdio.h>	  // printf
 #include <termios.h>  // struct termios, tcgetattr, tcsetattr
 #include <stdarg.h>   // va_list, va_start, va_end
+#include "../lib/utils.h"
 
 #define Width 10    // 游戏区域宽
 #define Height 20   // 游戏区域高
@@ -164,28 +165,6 @@ int check_hit(int x, int y, int rotate) {
     return c;
 }
 
-int getch(unsigned int timeout_ms=0) {
-    struct timeval timeout;
-    fd_set fds;
-    int ret;
-    char c;
-
-    FD_ZERO(&fds);
-    FD_SET(STDIN_FILENO, &fds);
-
-    timeout.tv_sec = timeout_ms / 1000;   // 秒部分
-    timeout.tv_usec = (timeout_ms % 1000) * 1000;  // 毫秒转换为微秒
-
-    ret = select(STDIN_FILENO + 1, &fds, NULL, NULL, &timeout);
-
-    if (ret > 0) {
-        if (read(STDIN_FILENO, &c, 1) > 0) {
-            return (unsigned char)c;  // 返回读取的字符
-        }
-    }
-    return -1; // 超时或错误
-}
-
 int do_tick() {
     if (++tick > 30) {
         tick = 0;
@@ -245,35 +224,6 @@ void runloop() {
         update_piece();
         draw_board();
     }
-}
-
-// 禁用缓冲输入时，用户输入的每个字符都会立即传递给程序，而不需要按Enter(用于实时交互)
-void setBufferedInput(bool enable)
-{
-	static bool enabled = true;
-	static struct termios old;
-	struct termios new_;
-
-	if (enable && !enabled)
-	{
-		// restore the former settings
-		tcsetattr(STDIN_FILENO, TCSANOW, &old);
-		// set the new state
-		enabled = true;
-	}
-	else if (!enable && enabled)
-	{
-		// get the terminal settings for standard input
-		tcgetattr(STDIN_FILENO, &new_);
-		// we want to keep the old setting to restore them at the end
-		old = new_;
-		// disable canonical mode (buffered i/o) and local echo
-		new_.c_lflag &= (~ICANON & ~ECHO);
-		// set the new_ settings immediately
-		tcsetattr(STDIN_FILENO, TCSANOW, &new_);
-		// set the new state
-		enabled = false;
-	}
 }
 
 void signal_callback_handler(int signum)
